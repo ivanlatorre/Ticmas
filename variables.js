6d4e04dbeec9f81acc3316f14785236e5d531c50
ivan <ivanlatorre720@gmail.com>
2023-01-05 00:47:11 -0300

    variables JS

---
+++ variables.js
@@ -0,0 +1,17 @@
+let longitud =16;
+let apellido = "Johnson"
+//let x = {nombre:}
+
+let x= 16 + "Volvo";
+//JS es no tipado
+
+// tipos de variables
+
+let z; //Definimos la variable z
+console.log(x);
+z = 5; // Z le asignamos un valor numérico
+console.log(x);
+z = "John" // Z ahora almacena un string
+console.log(x);
+
+
