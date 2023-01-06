e488405b65554e421b11f9ecd070b48052fab461
ivan <ivanlatorre720@gmail.com>
2023-01-06 20:56:47 -0300

    do while

--- while.js
+++ while.js
@@ -1,5 +1,12 @@
 var x = 10;
 while(x > 0){
-    console.log("Mi variable vale "+x );
+    console.log("Mi variable while vale "+x );
     x--;
 }
+
+
+var z = 10;
+//se ejecuta el codigo una vez y despues la condición
+do{
+    console.log("Mi variable do while vale: "+ z)
+}while(z > 10)
