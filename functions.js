4efecfa87766970d39059b3ea525d132eb1258c4
ivan <ivanlatorre720@gmail.com>
2023-01-07 14:46:34 -0300

    functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+function holaMundo(){
+    console.log("Hola Mundo");
+}
+
+
+
+holaMundo();
+
+function sumar(a,b){
+    var resultado = a+b;
+    return resultado = a+b;
+}
+
+
+suma = sumar(3,4);
+console.log(suma);
