aa9070bd74b0170e2b26f5badd6de296a7843e35
ivan <ivanlatorre720@gmail.com>
2023-01-08 02:18:37 -0300

    elementById y AddEventLIstener

--- js/index2.js
+++ js/index2.js
@@ -2,7 +2,15 @@
     console.log("Capturamos el evento click");//en consola
     document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
 }*/
+/*
 document.addEventListener('click', function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
+
+
+});*/
+
+document.getElementById('boton').addEventListener('click', function(){
     console.log("Hola mundo desde EventListener");
     document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
 
