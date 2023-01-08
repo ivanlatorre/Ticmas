ca40386a33967bf15c7c12a645abb56624fa28d7
ivan <ivanlatorre720@gmail.com>
2023-01-08 02:14:27 -0300

    EventListener

--- js/index2.js
+++ js/index2.js
@@ -1,4 +1,10 @@
-document.getElementById('boton'). onclick = function (){
+/*document.getElementById('boton'). onclick = function (){
     console.log("Capturamos el evento click");//en consola
     document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
-}
+}*/
+document.addEventListener('click', function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
+
+
+});
