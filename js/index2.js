a7e62d6e067ed30a03788990c31261ec113fb692
ivan <ivanlatorre720@gmail.com>
2023-01-08 02:06:32 -0300

    evento JS

--- js/index2.js
+++ js/index2.js
@@ -1,8 +1,4 @@
-console.log("Hola Mundo");
-
-let numero1 = 3;
-let numero2 = 4;
-
-let resultado = numero1 + numero2;
-
-console.log( "La suma de numero1 y numero es " + resultado);
+document.getElementById('boton'). onclick = function (){
+    console.log("Capturamos el evento click");//en consola
+    document.getElementById("demo").innerHTML = " Estamos probando nuestro primero elemento en  JS"
+}
