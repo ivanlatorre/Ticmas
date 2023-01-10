3be2734b9a19e1ad033cabe7378cdb8f80ef64c5
ivan <ivanlatorre720@gmail.com>
2023-01-10 01:01:44 -0300

    calculadora

---
+++ js/suma.js
@@ -0,0 +1,17 @@
+document.getElementById('sumar').addEventListener('click', function(){
+    
+    let numeroA = document.getElementById('numero1').value;
+    console.log('El valor del numero A es: '+numeroA);
+    let numeroB = document.getElementById('numero2').value;
+    console.log('El valor del numero A es: '+numeroB);
+
+    let resultado = sumar(numeroA, numeroB);
+    console.log('El resultado de la suma es: '+resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+    document.getElementById('contenedorResultado').style.display='block';
+
+});
+function sumar (a,b){
+    return a+b;
+}
